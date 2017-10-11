94351cae518f48098e9172d25168864bc08a59cd
Vitaliy Kovrov <virtaly@ya.ru>
2017-10-11 19:27:13 +0300

    1.3. JavaDoc. Code convention. [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j;
 
+/**
+* Class Calculate для вычисления арифметических операций
+* @author vkovrov
+* @version 0.1
+* @since 0.1
+*/
 public class Calculate {
+
+	/**
+	* Метод main точка входа в программу при запуске
+	* @param args - массив входных данных 
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
