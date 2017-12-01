70ac18f266c6aa712245f5b82b758a66a515335c
Vitaliy Kovrov <virtaly@ya.ru>
2017-12-01 13:18:53 +0300

    5.1.4. Создать convert(Iterator<Iterator>) [#152]

---
+++ chapter_005/src/main/java/ru/job4j/iteration/IteratorOfIterators.java
@@ -0,0 +1,64 @@
+package ru.job4j.iteration;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Класс для преобразования итератора итераторов целых чисел в итератор целых чисел.
+ * @author vkovrov
+ * @version 0.1
+ * @since 0.1
+ */
+public class IteratorOfIterators {
+
+    /**
+     * Метод для преобразования итератора итераторов целых чисел в итератор целых чисел.
+     * @param it итератор итераторов целых чисел.
+     * @return итератор целых чисел.
+     */
+    public Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+        return new Iterator<Integer>() {
+
+            /**
+             * Поле для итератора итераторов.
+             */
+            private Iterator<Iterator<Integer>> aIt = it;
+
+            /**
+             * Поле для указателя на текущий элемент в итераторе итераторов.
+             */
+            private Iterator<Integer> currentIterator = aIt.next();
+
+            /**
+             * Поле для указателя поиска элемента в методе hasNext.
+             */
+            private Iterator<Integer> fakeIterator = aIt.next();
+
+            /**
+             * Метод для определения наличия следующего элемента.
+             * @return есть ли следующий элемент.
+             */
+            @Override
+            public boolean hasNext() {
+                if (!fakeIterator.hasNext()) {
+                    fakeIterator = aIt.next();
+                }
+                boolean result = fakeIterator.hasNext();
+                fakeIterator = currentIterator;
+                return result;
+            }
+
+            /**
+             * Метод для возвращения следующего элемента.
+             * @return следующий элемент.
+             */
+            @Override
+            public Integer next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return currentIterator.next();
+            }
+        };
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/iteration/IteratorOfIteratorsTest.java
@@ -0,0 +1,105 @@
+package ru.job4j.iteration;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Тест для класса IteratorOfIterators.
+ * @author Vitaly Kovrov (mailto:virtaly@ya.ru)
+ * @version 0.1
+ * @since 0.1
+ */
+public class IteratorOfIteratorsTest {
+
+    /**
+     * Поле для итератора.
+     */
+    private Iterator<Integer> it;
+
+    /**
+     * Метод для создания итератора перед запуском тестов.
+     */
+    @Before
+    public void setUp() {
+        Iterator<Integer> it1 = Arrays.asList(1, 2, 3).iterator();
+        Iterator<Integer> it2 = Arrays.asList(4, 5, 6).iterator();
+        Iterator<Integer> it3 = Arrays.asList(0).iterator();
+        Iterator<Integer> it4 = Arrays.asList(7, 8, 9).iterator();
+        Iterator<Iterator<Integer>> its = Arrays.asList(it1, it2, it3, it4).iterator();
+        IteratorOfIterators converter = new IteratorOfIterators();
+        it = converter.convert(its);
+    }
+
+    /**
+     * Тест для методов next() и hasNext().
+     * Должен пройти все проверки и в конце выбросить исключение отсутствия следующего элемента.
+     */
+    @Test(expected = NoSuchElementException.class)
+    public void hasNextNextSequentialInvocation() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(5));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(6));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(7));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(8));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(9));
+        assertThat(it.hasNext(), is(false));
+        it.next();
+    }
+
+    /**
+     * Тест для метода next().
+     * Должен пройти все проверки и в конце выбросить исключение отсутствия следующего элемента.
+     */
+    @Test(expected = NoSuchElementException.class)
+    public void testsThatNextMethodDoesntDependsOnPriorHasNextInvocation() {
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(5));
+        assertThat(it.next(), is(6));
+        assertThat(it.next(), is(7));
+        assertThat(it.next(), is(8));
+        assertThat(it.next(), is(9));
+        it.next();
+    }
+
+    /**
+     * Тест для методов next() и hasNext().
+     * Должен пройти все проверки и в конце выбросить исключение отсутствия следующего элемента.
+     */
+    @Test(expected = NoSuchElementException.class)
+    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(5));
+        assertThat(it.next(), is(6));
+        assertThat(it.next(), is(7));
+        assertThat(it.next(), is(8));
+        assertThat(it.next(), is(9));
+        it.next();
+    }
+}
